450b06958a93ec64c746c818bf24da49c3ee1074
Rutvij <rutvij@pointzero.in>
2017-11-04 19:37:58 +0530

    Mongodb Routes

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 
 import App from './app'
@@ -10,17 +11,19 @@ import { APP_CONTAINER_SELECTOR } from '../shared/config';
 const rootEl = document.querySelector(APP_CONTAINER_SELECTOR);
 
 const wrapApp = AppComponent =>
-  <AppContainer>
-    <AppComponent />
-  </AppContainer>
+	<BrowserRouter>
+		<AppContainer>
+			<AppComponent />
+		</AppContainer>
+	</BrowserRouter>
 
 ReactDOM.render(wrapApp(App), rootEl)
 
 if (module.hot) {
-  // flow-disable-next-line
-  module.hot.accept('./app', () => {
-    // eslint-disable-next-line global-require
-    const NextApp = require('./app').default
-    ReactDOM.render(wrapApp(NextApp), rootEl)
-  });
+	// flow-disable-next-line
+	module.hot.accept('./app', () => {
+		// eslint-disable-next-line global-require
+		const NextApp = require('./app').default
+		ReactDOM.render(wrapApp(NextApp), rootEl)
+	});
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,19 +1,25 @@
 import compression from 'compression';
 import express from 'express';
 import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
 
 import { APP_NAME, STATIC_PATH, WEB_PORT, MONGO_URI } from '../shared/config';
 import { isProd } from '../shared/util';
 import renderApp from './render-app';
 
+import api from './api';
 
 const app = express();
 
 app.use(compression());
 app.use(STATIC_PATH, express.static('dist'));
 app.use(STATIC_PATH, express.static('public'));
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-app.get('/', (req, res) => {
+api(app, {});
+
+app.get('/*', (req, res) => {
   res.send(renderApp(APP_NAME));
 });
 
@@ -21,7 +27,7 @@ app.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
   '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`);
-  MongoClient.connect(isProd ? MONGO_URI : 'mongodb://localhost:27017', function(err, db) {
+  MongoClient.connect(isProd ? MONGO_URI : 'mongodb://localhost:27017/test', function(err, db) {
   	console.log("DB Connected");
 	});
 });
